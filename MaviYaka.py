4b33cead1eb01348a45e1bd57c640375453adfb8
ahmetakdmrrr <119605576+ahmetakdmrrr@users.noreply.github.com>
2023-05-27 14:50:17 +0300

    Update MaviYaka.py
    init methodu ve get ,set fonksiyonları tanımlandı

--- MaviYaka.py
+++ MaviYaka.py
@@ -1,3 +1,25 @@
 from Calisan import Calisan
 class MaviYaka(Calisan):
-    pass
+
+    def __init__(self, ad, soyad, maas, tecrube):
+
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__maaş = maas
+        self.__tecrübe = tecrube
+        self.__zam_hakki = self.zam_hakkı()
+
+    def get_maaş(self):
+        return self.__maaş
+
+
+    def get_tecrübe(self):
+        return self.__tecrübe
+
+
+    def set_maaş(self, maaş):
+        self.__maaş = maaş
+
+
+    def set_tecrübe(self, tecrübe):
+        self.__tecrübe = tecrübe
